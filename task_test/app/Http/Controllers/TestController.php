b71860c2bd56cc589ae287d03f0ecca8e91104b3
seomy <muyear.seo@gmail.com>
2022-10-25 18:32:41 +0900

    laravel test

--- task_test/app/Http/Controllers/TestController.php
+++ task_test/app/Http/Controllers/TestController.php
@@ -4,9 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
+
 class TestController extends Controller
 {
     public function index() {
-        return view('tests.test');
+
+
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb');
+
+        //クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
+        ->select('id', 'text')
+        ->get();
+
+
+
+
+        dd($queryBuilder);
+
+
+
+
+        return view('tests.test', compact('values'));
     }
 }
--- task_test/routes/web.php
+++ task_test/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,5 +16,9 @@ use App\Http\Controllers\TestController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('tests/test', [TestController::class, 'index']);
-        
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
